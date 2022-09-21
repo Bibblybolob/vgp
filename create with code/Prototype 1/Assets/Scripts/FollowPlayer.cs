cc7b567a12e9133182d27c93bee1d3e9d7e5c34a
Bibblybolob <jonathantran365@gmail.com>
2022-09-21 10:41:44 -0700

    hekklo

--- create with code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ create with code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController1 : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -27);
     // Start is called before the first frame update
     void Start()
     {
@@ -11,11 +13,8 @@ public class PlayerController1 : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 5.0F;
-
-    void Update()
+    void LateUpdate()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
