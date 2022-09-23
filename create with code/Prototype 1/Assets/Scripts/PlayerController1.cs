91c912f185bbd90efaafe4a77e8e53e25a6d9cca
Bibblybolob <jonathantran365@gmail.com>
2022-09-23 10:41:29 -0700

    das

--- create with code/Prototype 1/Assets/Scripts/PlayerController1.cs
+++ create with code/Prototype 1/Assets/Scripts/PlayerController1.cs
@@ -11,11 +11,17 @@ public class PlayerController1 : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 5.0F;
+    public float speed = 20.0F;
+    public float turnSpeed = 45.0F;
+    public float horizontalInput;
+    public float forwardInput;
 
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Moves the car forward based on verticle input
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
     }
 }
