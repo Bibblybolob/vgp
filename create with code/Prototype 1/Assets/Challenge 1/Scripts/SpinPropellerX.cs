d3945dbfaf6a2995b677861521c6355bf37348c5
Bibblybolob <jonathantran365@gmail.com>
2022-10-05 10:32:57 -0700

    xczxc

---
+++ create with code/Prototype 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float propellerSpeed = 1000F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellerSpeed * Time.deltaTime);
+    }
+}
