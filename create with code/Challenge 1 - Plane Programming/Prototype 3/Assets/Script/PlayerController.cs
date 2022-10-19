299064634cc0a85c8296277a1c0016f45bdeb100
Bibblybolob <jonathantran365@gmail.com>
2022-10-19 09:27:23 -0700

    sd

---
+++ create with code/Challenge 1 - Plane Programming/Prototype 3/Assets/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float gravityModifier;
+    public float jumpForce;
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+       Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround){
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        isOnGround = false;
+    }
+}
