31bb3b89a06c11376aad6f7f29b3348d715a7c8f
Bibblybolob <jonathantran365@gmail.com>
2022-11-14 10:18:11 -0800

    d

--- create with code/Challenge 1 - Plane Programming/Prototype 3/Assets/Script/PlayerController.cs
+++ create with code/Challenge 1 - Plane Programming/Prototype 3/Assets/Script/PlayerController.cs
@@ -9,20 +9,25 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     
@@ -35,6 +40,11 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1); {
+            explosionParticle.Play();
+            {dirtParticle.Stop(); }
         }
     }
+}
 }
