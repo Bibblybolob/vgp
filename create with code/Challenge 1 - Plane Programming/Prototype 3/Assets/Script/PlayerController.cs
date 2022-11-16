a1d8bf2343aa43807fd5003c3f6ccbd0168464de
Bibblybolob <jonathantran365@gmail.com>
2022-11-16 10:39:47 -0800

    dsd

--- create with code/Challenge 1 - Plane Programming/Prototype 3/Assets/Script/PlayerController.cs
+++ create with code/Challenge 1 - Plane Programming/Prototype 3/Assets/Script/PlayerController.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float gravityModifier;
-    public float jumpForce;
-    public bool isOnGround = true;
-    public bool gameOver = false;
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
        Physics.gravity *= gravityModifier;
+       playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,9 +29,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+             playerAudio.PlayOneShot(jumpSound, 1.0f);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
     
@@ -35,16 +41,17 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-        isOnGround = true;
+            isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle")) 
         {
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1); {
+            playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
-            {dirtParticle.Stop(); }
+            dirtParticle.Stop();
         }
     }
-}
 }
