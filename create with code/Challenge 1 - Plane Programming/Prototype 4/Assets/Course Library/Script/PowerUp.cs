0c6ce302bc6321c55a51df296387b1f21975e292
Bibblybolob <168976@student.vansd.org>
2023-02-27 09:56:22 -0800

    g

---
+++ create with code/Challenge 1 - Plane Programming/Prototype 4/Assets/Course Library/Script/PowerUp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerUpType { None, Pushback, Rockets }
+
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType powerUpType;
+}
---
+++ create with code/Challenge 1 - Plane Programming/Prototype 4/Assets/Course Library/Script/RocketBehavior.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehavior : MonoBehaviour
+{
+    private Transform target;
+    private float speed = 15.0f;
+    private bool homing;
+    
+    private float rocketStrength = 15.0f;
+    private float aliveTimer = 5.0f;
+
+    void Update()
+    {
+        if (homing && target != null)
+        {
+            Vector3 moveDirection = (target.transform.position - transform.position).normalized;
+            transform.position += moveDirection * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }
+    }
+    public void Fire (Transform newTarget)
+    {
+        target = homingTarget;
+        homing = true;
+        Destroy(gameObject, aliveTimer);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (target != null)
+        {
+            if (col.gameObject.CompareTag(target.tag))
+            {
+                Rigidbody targetRigidbody = col.gameObject.GetComponent<Rigidbody>();
+                Vector3 away = -col.contacts[0].normal;
+                targetRigidbody.AddForce(away * rocketStrength, ForceMode.Impulse);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
